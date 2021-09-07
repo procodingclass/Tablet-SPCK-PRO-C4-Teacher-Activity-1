87208c3243bc0316cb3941ee3743b1fe0cc3d147
procodingclass@whitehatjr.com <procodingclass@whitehatjr.com>
2021-09-07 11:31:24 +0530

    Revamped Version 1

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 var input, heading;
-
 function setup() {
   createCanvas(400, 400);
-  background(178, 255, 102);
+
+  input = createInput();
+  input.position(5, 60);
+
+  heading = createElement("h4", "Enter any alphabet:");
+  heading.position(5, 20);
+
+  textAlign(CENTER);
+  textSize(50);
 }
 
-function draw() {}
+function draw() {
+  background("orange");
+
+  //generate random alphbet
+  var value = input.value();
+
+  // Write Code for switch statement
+}
